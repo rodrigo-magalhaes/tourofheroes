bb3b7680e2fe23ba12f4eb16735b255b77ee0b78
me <me@me>
2017-06-04 12:41:00 -0300

    Multiple Components step finished!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from "app/hero";
 
-export class Hero {
-  id: number;
-  name: string;
-}
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
@@ -12,8 +21,10 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heros';
-  hero: Hero = {
-    id: 1,
-    name: 'windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
